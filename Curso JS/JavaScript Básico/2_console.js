571d6d7503484339e98d19854cf6acaf85c31509
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-05-14 15:29:35 -0400

    JavaScript Básico
    Comentário, Console.lor e HTML e Javascript

---
+++ Curso JS/JavaScript Básico/2_console.js
@@ -0,0 +1,8 @@
+console.log('Francisco Oliveira "Braga"')
+console.log("Francisco Oliveira 'Braga'")
+console.log(`Francisco Oliveira Braga`)
+let idade = 42
+console.log(`Francisco tem  ${idade} anos`)
+console.log("Francisco Oliveira 'Braga' tem "+ idade + " anos")
+
+console.log(12, 30.67, 'Francisco Braga')
---
+++ Curso JS/JavaScript Básico/3_comentario.js
@@ -0,0 +1,18 @@
+//Comentário de linha
+/*comentário 
+de 
+bloco
+*/
+
+console.log("Comentário como fazer")
+// com duas barras invertidas é um comentário de bloco
+console.log("Comentários são ingnorados pelo interpretador do javascript")
+//console.log("Eu sou um exemplo, execute que eu não apareço")
+console.log("entendeu, você leu o comentário acima")
+/*let valor = 100
+console.log(valor)
+*/
+console.log("entendeu, você leu o comentário acima")
+
+
+
