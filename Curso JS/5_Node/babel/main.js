2f1314073a6ec23a59cf506cb7fe8e47988cdae4
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-07-03 12:23:06 -0400

    estudos sobre babel e webpack
    Configurando babel e webpack para navegadores antigos possam estar executando o javascript sem problemas

---
+++ Curso JS/5_Node/babel/main.js
@@ -0,0 +1,15 @@
+/* const nome = 'Luiz'
+const obj ={
+    noem
+}
+const novoOBJ ={
+    ...obj
+}
+console.log(novoOBJ) */
+
+class Pessoa{
+    constructor(nome,sobrenome){
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+}
